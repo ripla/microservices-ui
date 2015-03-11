db4e699018c95ac7daa91fe6d4b05ba11684fb65
Risto Yrjänä <risto@vaadin.com>
2015-03-11 20:24:02 +0200

    Disable memcache for now

--- src/main/java/org/vaadin/risto/microservices/ApplicationCloudConfiguration.java
+++ src/main/java/org/vaadin/risto/microservices/ApplicationCloudConfiguration.java
@@ -9,8 +9,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("cloud")
+//Disabled until vaadin4spring is serializable
+//@Configuration
+//@Profile("cloud")
 public class ApplicationCloudConfiguration {
 
     @Value("${memcache-url}")
